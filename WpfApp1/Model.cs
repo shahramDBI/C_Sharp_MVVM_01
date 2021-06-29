74fe57a4e8b13e6143319852ac321b85c24805d3
shahramDBI <shahramdarbandi79@gmail.com>
2021-06-29 16:48:32 +0430

    Finish

---
+++ WpfApp1/Model.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace WpfApp1
+{
+    class Model:INotifyPropertyChanged
+    {
+        List<ViewModel> List = new List<ViewModel>();
+        public Model()
+        {
+            List.Add(new ViewModel { id = 1, name = "Shahram", tel = "123456789", address = "Tehran" });
+            List.Add(new ViewModel { id = 2, name = "Shahrooz", tel = "123456789", address = "Shiraz" });
+            List.Add(new ViewModel { id = 3, name = "Shervin", tel = "123456789", address = "Mashhad" });
+            List.Add(new ViewModel { id = 4, name = "Asali", tel = "123456789", address = "Bandar" });
+        }
+        private int ID;
+        public int id
+        {
+            get
+            {
+                return ID;
+            }
+            set
+            {
+                ID = value;
+                Search(ID);
+                NotifyPropertyChanged("id");
+            }
+        }
+        
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string pr)
+        {
+            if(PropertyChanged !=null)
+            {
+                PropertyChanged(this,new PropertyChangedEventArgs(pr));
+            }
+        }
+
+        private ViewModel VM;
+        public ViewModel vm
+        {
+            get
+            {
+                return VM;
+            }
+            set
+            {
+                VM = value;
+                NotifyPropertyChanged("vm");
+            }
+        }
+        public void Search(int ID)
+        {
+            vm = List.Where(x => x.id == ID).Single();
+        }
+
+    }
+}
---
+++ WpfApp1/ViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class ViewModel
+    {
+        private int ID;
+        public int id
+        {
+            get
+            {
+                return ID;
+            }
+            set
+            {
+                ID = value;
+            }
+        }
+        private string Name;
+        public string name
+        {
+            get
+            {
+                return Name;
+            }
+            set
+            {
+                Name = value;
+            }
+        }
+        private string Tel;
+        public string tel
+        {
+            get
+            {
+                return Tel;
+            }
+            set
+            {
+                Tel = value;
+            }
+        }
+        private string Address;
+        public string address
+        {
+            get
+            {
+                return Address;
+            }
+            set
+            {
+                Address = value;
+            }
+        }
+    }
+}
